d744845bd5e95f1089160aa15a6d5720e51eedf0
Pivi Lartisant <paul.lartisant@icloud.com>
2022-10-26 13:58:18 +0200

    styling

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,9 @@
 /*======================
 Creating my DOM elements
 ========================*/
-const title = document.createElement('h1');
-  title.textContent = `Cyber Tarot App`;
-  document.body.appendChild(title);
+// const title = document.createElement('h1');
+//   title.textContent = `Spooky Tarot App`;
+//   document.body.appendChild(title);
 
   const btn = document.createElement('button');
   btn.innerHTML = "Generate spread"
@@ -12,27 +12,33 @@ const title = document.createElement('h1');
 
 const cardName = document.createElement('div');
   cardName.setAttribute('id','cardName');
+  cardName.setAttribute('class','card-name');
   document.body.appendChild(cardName)
 
 const cardArcana = document.createElement('div');
   cardArcana.setAttribute('id','cardArcana');
+  cardArcana.setAttribute('class','card-arcana');
   document.body.appendChild(cardArcana)
 
 const cardFortune = document.createElement('div');
   cardFortune.setAttribute('id','cardFortune');
+  cardFortune.setAttribute('class','card-fortune');
   document.body.appendChild(cardFortune)
 
 const cardKeywords = document.createElement('div');
   cardKeywords.setAttribute('id','cardKeywords');
+  cardKeywords.setAttribute('class','card-keywords');
   document.body.appendChild(cardKeywords)
 
 const cardMeaning = document.createElement('div');
   cardMeaning.setAttribute('id','cardMeaning');
+  cardMeaning.setAttribute('class','card-meaning');
   document.body.appendChild(cardMeaning)
 
 
 const cardImg = document.createElement('img');
   cardImg.setAttribute('id','cardImg');
+  cardImg.setAttribute('class','card-img');
   // cardImg.src="./data/cardImg/c01.jpg";
   document.body.appendChild(cardImg)
 
---
+++ scripts/burgerMenu.js
@@ -0,0 +1,37 @@
+//Declaring my variables
+const burgerMenu = document.getElementById('burger-menu')
+const nav = document.getElementById('nav-link')
+const burgerIcon = document.getElementById('menu-icon')
+
+const navLink1 = document.getElementById('home');
+const navLink2 = document.getElementById('search');
+const navLink3 = document.getElementById('about');
+const navLink4 = document.getElementById('support');
+
+//Burgermenu click event listener
+burgerMenu.addEventListener('click',()=>burgerClick());
+
+//Burger menu function which toggle a couple of different classes
+
+function burgerClick (){
+    burgerMenu.classList.toggle('active')
+    nav.classList.toggle('active')
+    burgerIcon.classList.toggle('toggled')
+}
+
+
+
+//click on nav event listeners 
+navLink1.addEventListener('click',()=> navClick());
+navLink2.addEventListener('click',()=> navClick());
+navLink3.addEventListener('click',()=> navClick());
+
+//this function removes a number of the classes that we're previously added 
+function navClick (){
+    burgerMenu.classList.remove('active')
+    burgerTop.classList.remove('toggled')
+    burgerMid.classList.remove('toggled')
+    burgerBot.classList.remove('toggled')
+    nav.classList.remove('active')
+}
+
