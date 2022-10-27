6f1400cb65fcc7aa644e1d71e5b69f7bc4eed8cb
Pivi Lartisant <paul.lartisant@icloud.com>
2022-10-27 22:09:08 +0200

    initial

---
+++ scripts/ahha.js
@@ -0,0 +1,11 @@
+const audioBtn = document.getElementById('spread')
+
+var audio = new Audio('../assets/thriller.mp3');
+
+window.addEventListener('mouseover',()=>
+audio.play())
+
+// console.log('audio')
+
+// audio.play()
+
