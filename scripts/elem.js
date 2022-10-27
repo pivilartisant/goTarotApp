5f6209f63864faf30d51e97ea6b75acc0326ea0a
Pivi Lartisant <paul.lartisant@icloud.com>
2022-10-27 20:38:02 +0200

    new doc

---
+++ scripts/elem.js
@@ -0,0 +1,35 @@
+// const btn = document.createElement('button');
+// btn.innerHTML = "Generate spread"
+// btn.setAttribute('id','spread');
+// document.body.appendChild(btn);
+
+// const cardName = document.createElement('div');
+// cardName.setAttribute('id','cardName');
+// cardName.setAttribute('class','card-name');
+// document.body.appendChild(cardName)
+
+// const cardArcana = document.createElement('div');
+// cardArcana.setAttribute('id','cardArcana');
+// cardArcana.setAttribute('class','card-arcana');
+// document.body.appendChild(cardArcana)
+
+// const cardFortune = document.createElement('div');
+// cardFortune.setAttribute('id','cardFortune');
+// cardFortune.setAttribute('class','card-fortune');
+// document.body.appendChild(cardFortune)
+
+// const cardKeywords = document.createElement('div');
+// cardKeywords.setAttribute('id','cardKeywords');
+// cardKeywords.setAttribute('class','card-keywords');
+// document.body.appendChild(cardKeywords)
+
+// const cardMeaning = document.createElement('div');
+// cardMeaning.setAttribute('id','cardMeaning');
+// cardMeaning.setAttribute('class','card-meaning');
+// document.body.appendChild(cardMeaning)
+
+// const cardImg = document.createElement('img');
+// cardImg.setAttribute('id','cardImg');
+// cardImg.setAttribute('class','card-img');
+// // cardImg.src="./data/cardImg/c01.jpg";
+// document.body.appendChild(cardImg)
